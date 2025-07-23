f28d8ec328fe1ae9a316be83f98a960758952139
JunSaif <douidysifeddine@gmail.com>
2025-07-23 13:01:20 +0100

    change

--- live/my-project/providers.tf
+++ live/my-project/providers.tf
@@ -7,14 +7,7 @@ terraform {
   }
 }
 provider "aws" {
-  # region = var.aws_region
-  region                      = "us-east-1"
-  access_key                  = "foo"
-  secret_key                  = "bar"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_region_validation      = true
-  skip_requesting_account_id  = true
+  region = var.aws_region
 
   default_tags {
     tags = local.common_tags
